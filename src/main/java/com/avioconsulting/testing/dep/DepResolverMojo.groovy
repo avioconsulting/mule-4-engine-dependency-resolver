87b972089e2b1a1f4927396eb1f64e94f65e57d0
Brady Wied <brady@bswtechconsulting.com>
2018-10-20 21:06:39 -0600

    More progress

--- src/main/java/com/avioconsulting/testing/dep/DepResolverMojo.groovy
+++ src/main/java/com/avioconsulting/testing/dep/DepResolverMojo.groovy
@@ -17,13 +17,24 @@ class DepResolverMojo extends
     @Component
     private MavenProject mavenProject
 
-    Map getDependencyMap(Set<Artifact> artifacts) {
-        artifacts.inject([:]) { map, artifact ->
-            map
+    Map getDependencyMap(Set<Artifact> artifacts,
+                         Map results = [:]) {
+        artifacts.each { artifact ->
+            results[withoutScope(artifact)] = [
+                    groupId   : artifact.groupId,
+                    artifactId: artifact.artifactId,
+                    version   : artifact.version,
+                    filename  : artifact.file
+            ]
         }
+        results
 //        artifacts.each { a ->
 //            println "artifact ${a.artifactId} file ${a.file} - deps - ${a.getDependencyTrail()} - tostring ${a.toString()} scope - ${a.scope}"
 //        }
+    }
+
+    private static String withoutScope(Artifact artifact) {
+        "${artifact.groupId}:${artifact.artifactId}:${artifact.version}"
     }
 
     @Override
