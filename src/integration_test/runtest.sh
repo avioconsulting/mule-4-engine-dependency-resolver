10ccec5f458e310f27f9aa72dc778cb50a7352ba
Brady Wied <brady@bswtechconsulting.com>
2018-10-22 16:03:31 -0600

    Use naming convention

--- src/integration_test/runtest.sh
+++ src/integration_test/runtest.sh
@@ -6,6 +6,6 @@ mvn clean install -DskipTests
 #mkdir -p $ARTIFACT_DIR
 #cp -v target/depresolver-1.0.0.jar $ARTIFACT_DIR
 #cp -v pom.xml $ARTIFACT_DIR/depresolver-1.0.0.pom
-#mvn -Dmaven.repo.local=temporary_repo com.avioconsulting.mule:depresolver:1.0.0:resolve -Dresolve.dependencies.comma.separated=org.apache.maven:maven-core:3.5.4 -Dresolve.sort.output=true
-mvn com.avioconsulting.mule:depresolver:1.0.0:resolve -Dresolve.dependencies.comma.separated=org.apache.maven:maven-core:3.5.4  -Dresolve.sort.output=true -Dresolve.outputFile=target/stuff.json
+#mvn -Dmaven.repo.local=temporary_repo
+mvn com.avioconsulting.mule:dependency-resolver-maven-plugin:1.0.0:resolve -Dresolve.dependencies.comma.separated=org.apache.maven:maven-core:3.5.4  -Dresolve.sort.output=true -Dresolve.outputFile=target/stuff.json
 diff src/integration_test/dependencies.json target/stuff.json
